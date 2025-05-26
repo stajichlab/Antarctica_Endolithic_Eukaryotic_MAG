545c6b3f6fed7b090d1e8ad7cffc09caeae02ee5
Jason Stajich <jasonstajich.phd@gmail.com>
2025-05-25 21:13:55 -0700

    orthofinder

---
+++ Novel_Pezizo/pipeline/orthofinder/01_orthofinder.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash -l
+#SBATCH -p epyc -c 48 -N 1 -n 1 --out logs/orthofinder.log
+
+module load orthofinder
+CPU=2
+
+if [ $SLURM_CPUS_ON_NODE ]; then
+ CPU=$SLURM_CPUS_ON_NODE
+fi
+
+time orthofinder -f input_orthofinder -t $CPU -a 16 -o OrthoFinder
