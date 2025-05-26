73b065f7850923713e2c65949c6551ee544f8dae
Jason Stajich <jasonstajich.phd@gmail.com>
2025-05-26 13:24:59 -0700

    don't gave the selected set in a subfolder anymore

--- Novel_Pezizo/pipeline/phylo.custom/04_make_concatpartition.sh
+++ Novel_Pezizo/pipeline/phylo.custom/04_make_concatpartition.sh
@@ -13,18 +13,18 @@ for type in ascomycota
 do
 	FILTERDIR=${type}_msa_filter
 	STEM=${type}
-	if [ ! -d $FILTERDIR/selected_MSAs ]; then
+	if [ ! -d $FILTERDIR ]; then
 		echo "no $FILTERDIR/selected_MSAs folder"
 		continue
 	fi
-	pushd $FILTERDIR/selected_MSAs
+	pushd $FILTERDIR
 	ls *.mfa > filenames
-	mkdir -p ../../${FILTERDIR}-buildtree
-	phykit create_concat -a filenames -p ../../$FILTERDIR-buildtree/${PREFIX}.${STEM}
+	mkdir -p ../${FILTERDIR}-buildtree
+	phykit create_concat -a filenames -p ../$FILTERDIR-buildtree/${PREFIX}.${STEM}
 	popd
 	pushd $FILTERDIR-buildtree
 	perl -i -p -e 's/AUTO/PROT/' ${PREFIX}.${STEM}.partition
 	#sbatch -p stajichlab -c $CPURUN --mem 128gb -J modeltest$type --out modeltest-${type}.%A.log --wrap "module load modeltest-ng; modeltest-ng -i ${PREFIX}.${STEM}.fa -q ${PREFIX}.${STEM}.partition --processes $CPURUN -T raxml -d aa -t user -u $USERTREE -T raxml"
-	sbatch -p stajichlab -c $CPURUN --mem 128gb -J modeltest$type --out modeltest-${type}.%A.log --wrap "hostname; module load modeltest-ng; modeltest-ng -i ${PREFIX}.${STEM}.fa -q ${PREFIX}.${STEM}.partition --processes $CPURUN -T raxml -d aa -t mp -T raxml"
+	sbatch -p epyc -c $CPURUN --mem 128gb -J modeltest$type --out modeltest-${type}.%A.log --wrap "hostname; module load modeltest-ng; modeltest-ng -i ${PREFIX}.${STEM}.fa -q ${PREFIX}.${STEM}.partition --processes $CPURUN -T raxml -d aa -t mp -T raxml"
 	popd
 done
